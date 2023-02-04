2b08156e4d1cae5214c92dcac9f8ed9c4c3438d2
GitXBot101 <117470125+GitXBot101@users.noreply.github.com>
2023-02-04 17:27:48 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -181,7 +181,7 @@ def start(update, context):
         buttons.buildbutton(f"{config_dict['START_BTN2_NAME']}", f"{config_dict['START_BTN2_URL']}")
     reply_markup = buttons.build_menu(2)
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
-        start_string = f'''This bot can mirror all your links to Google Drive!
+        start_string = f'''This bot can leech all your links to Telegram!
 Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
         if config_dict['PICS']:
